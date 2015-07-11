fd0da81e91ecd66831d7e83d981f1712af9f66e4
Ivorankov <ivo_rankov@mail.bg>
2015-07-11 20:13:39 +0300

    Homework

---
+++ 05. Variables, Data, Expressions and Constants/Homework/Variables, Data, Expressions and Constants/Task 1. Class Size in CSharp/Figure.cs
@@ -0,0 +1,30 @@
+﻿namespace Figure
+{
+    using System;
+
+    public class Figure
+    {
+        public Figure(double width, double heigth)
+        {
+            this.Width = width;
+            this.Heigth = heigth;
+        }
+
+        public double Width { get; private set; }
+
+        public double Heigth { get; private set; }
+
+        public static Figure GetRotatedFigure(Figure size, double angleOfTheFigureThatWillBeRotated)
+        {
+            double cos = Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotated));
+            double sin = Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotated));
+
+            double newWidth = (cos * size.Width) + (sin * size.Heigth);
+            double newHeigth = (sin * size.Width) + (cos * size.Heigth);
+
+            Figure newFigure = new Figure(newWidth, newHeigth);
+
+            return newFigure;
+        }
+    }
+}
---
+++ 05. Variables, Data, Expressions and Constants/Homework/Variables, Data, Expressions and Constants/Task 2. Method PrintStatistics in CSharp/PrintStatistics.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_2.Method_PrintStatistics_in_CSharp
+{
+    using System;
+
+   internal static class PrintStatistic
+    {
+        public static void PrintStatistics(double[] collectionOfStatistics, int amountOfStatistics)
+        {
+            double maxValue = collectionOfStatistics[0];
+            for (int i = 0; i < amountOfStatistics; i++)
+            {
+                if (collectionOfStatistics[i] > maxValue)
+                {
+                    maxValue = collectionOfStatistics[i];
+                }
+            }
+
+            PrintToConsole(maxValue);
+
+            double minValue = 0;
+            for (int i = 0; i < amountOfStatistics; i++)
+            {
+                if (collectionOfStatistics[i] < minValue)
+                {
+                    minValue = collectionOfStatistics[i];
+                }
+            }
+
+            PrintToConsole(minValue);
+
+            double sumOfStatistics = 0;
+            for (int i = 0; i < amountOfStatistics; i++)
+            {
+                sumOfStatistics += collectionOfStatistics[i];
+            }
+
+            double averageValue = sumOfStatistics / amountOfStatistics;
+            PrintToConsole(averageValue);
+        }
+
+        public static void PrintToConsole(double value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
