da66b75f8457191c96bb00f9475dc567dc1e8d49
Ivorankov <ivo_rankov@mail.bg>
2015-07-13 20:58:04 +0300

    fix

--- 07. High-quality Methods/Homework/Problem 1 - Calculation Problem/Calculations.cs
+++ 07. High-quality Methods/Homework/Problem 1 - Calculation Problem/Calculations.cs
@@ -12,7 +12,8 @@
             List<string> numbersIn23NumeralSystem = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w" };
 
             var input = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                .Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
 
             string[] inputArray = new string[input.Length];
             int resultInDecimal = 0;
