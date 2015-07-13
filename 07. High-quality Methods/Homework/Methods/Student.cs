7b25aa4f91fe904a81733f70489c5b5df3badcce
Ivorankov <ivo_rankov@mail.bg>
2015-07-13 20:48:46 +0300

    homework

---
+++ 07. High-quality Methods/Homework/Methods/Student.cs
@@ -0,0 +1,94 @@
+﻿namespace Methods
+{
+    using System;
+    using System.Globalization;
+
+    internal class Student
+    {
+        public Student(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+        }
+
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+        private string personalInfo;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            private set
+            {
+                this.ValidateNameLength(1, 12, value);
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            private set
+            {
+                this.ValidateNameLength(1, 12, value);
+                this.lastName = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return this.dateOfBirth;
+            }
+
+            private set
+            {
+                this.dateOfBirth = value;
+            }
+        }
+
+        public string PersonalInfo
+        {
+            get
+            {
+                return this.personalInfo;
+            }
+
+            set
+            {
+                this.personalInfo = value;
+            }
+        }
+
+        public bool IsOlderThan(Student other)
+        {
+            bool isOlder = this.DateOfBirth < other.DateOfBirth;
+            return isOlder;
+        }
+
+        private void ValidateNameLength(int minLength, int maxLength, string value)
+        {
+            bool isValid = minLength < value.Length && value.Length < maxLength;
+
+            if (isValid)
+            {
+                return;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Invalid name length");
+            }
+        }
+    }
+}
---
+++ 07. High-quality Methods/Homework/Problem 1 - Calculation Problem/Calculations.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+   internal class Calculations
+    {
+        static void Main()
+        {
+            const int NumeralSystem = 23;
+            List<string> numbersIn23NumeralSystem = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w" };
+
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] inputArray = new string[input.Length];
+            int resultInDecimal = 0;
+            int decimalNum = 0;
+            foreach (var item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    string twentyTreeDigit = item.Substring(i, 1);
+                    int number = numbersIn23NumeralSystem.IndexOf(twentyTreeDigit);
+
+                    decimalNum *= NumeralSystem;
+                    decimalNum += number;
+                }
+
+                resultInDecimal += decimalNum;
+                decimalNum = 0;
+            }
+
+            int tempDecimalResult = resultInDecimal;
+            int digit = 0;
+            StringBuilder twentyThreeNumber = new StringBuilder();
+            do
+            {
+                digit = tempDecimalResult % NumeralSystem;
+                tempDecimalResult /= NumeralSystem;
+                twentyThreeNumber.Insert(0, numbersIn23NumeralSystem[digit]);
+            } while (tempDecimalResult > 0);
+
+            Console.WriteLine(twentyThreeNumber + " = " + resultInDecimal);
+        }
+    }
+}
